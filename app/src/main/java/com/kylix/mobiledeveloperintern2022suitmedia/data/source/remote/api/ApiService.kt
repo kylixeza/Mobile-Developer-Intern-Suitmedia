a8b22fd0c0659e4d87e663e89a2d3c73a486558f
ACER <loilyxtra@gmail.com>
2022-09-14 22:29:34 +0700

    code clean up

--- app/src/main/java/com/kylix/mobiledeveloperintern2022suitmedia/data/source/remote/api/ApiService.kt
+++ app/src/main/java/com/kylix/mobiledeveloperintern2022suitmedia/data/source/remote/api/ApiService.kt
@@ -2,7 +2,6 @@ package com.kylix.mobiledeveloperintern2022suitmedia.data.source.remote.api
 
 import com.kylix.mobiledeveloperintern2022suitmedia.base.BaseResponse
 import com.kylix.mobiledeveloperintern2022suitmedia.data.source.remote.response.UserResponse
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
--- app/src/main/java/com/kylix/mobiledeveloperintern2022suitmedia/data/util/RemoteResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.kylix.mobiledeveloperintern2022suitmedia.data.util
-
-sealed class RemoteResponse<out T> {
-    data class Success<out T> (val data: T): RemoteResponse<T>()
-    data class Error(val errorMessage: String): RemoteResponse<Nothing>()
-    data class Empty(val message: String = ""): RemoteResponse<Nothing>()
-}
